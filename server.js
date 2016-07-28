16284afdcfad06e44b96e093199dc73f94b22426
zebesta <chris.sebesta@gmail.com>
2016-07-28 14:56:24 -0400

    experiements

--- server.js
+++ server.js
@@ -15,6 +15,35 @@ app.get('/', function (req, res, next) {
   }
 })
 
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+app.get('/what', function (req, res, next) {
+	console.log("what");
+	res.send('what');
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
